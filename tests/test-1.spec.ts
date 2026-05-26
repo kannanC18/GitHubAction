6aeb3367038c5864bb690141ea032f4449b5ffaf
kannanC18 <kannan.viru005@gmail.com>
2026-05-26 13:22:24 +0530

    Add console log for commit in pipeline

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -8,4 +8,5 @@ test('has title', async ({page}) => {
   await page.getByRole('textbox', {name: 'Password'}).fill('crmsfa');
   await page.getByRole('button', {name: 'Login'}).click();
   await expect(page.locator('#errorDiv')).toContainText('following error occurred during login:user not found.');
+  console.log(commit in pipeline);
 });
