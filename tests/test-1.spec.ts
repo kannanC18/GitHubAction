e43931d67020f3f4fe9c1230c04dd67e7b1cd7b6
Kannan Chinnapaiyan <kannan.viru005@gmail.com>
2026-05-26 12:45:41 +0530

    version new

---
+++ tests/test-1.spec.ts
@@ -0,0 +1,11 @@
+import {test, expect} from '@playwright/test';
+
+test('has title', async ({page}) => {
+  await page.goto('https://leaftaps.com/opentaps/control/main');
+  await page.getByRole('textbox', {name: 'Username'}).click();
+  await page.getByRole('textbox', {name: 'Username'}).fill('Demosalesmanager');
+  await page.getByRole('textbox', {name: 'Password'}).click();
+  await page.getByRole('textbox', {name: 'Password'}).fill('crmsfa');
+  await page.getByRole('button', {name: 'Login'}).click();
+  await expect(page.locator('#errorDiv')).toContainText('following error occurred during login:user not found.');
+});
