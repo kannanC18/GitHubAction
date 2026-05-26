a6d81d274bb768ffa8857650e0ec15cf6cae603f
kannanC18 <kannan.viru005@gmail.com>
2026-05-26 13:15:52 +0530

    Add comment for StorageState Implementation
    StroageState

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -15,5 +15,7 @@ test('get started link', async ({ page }) => {
 
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-  console.log("code pushed to git successfully");
+  console.log("code pushed to git successfully");\
+
+//Storagestate() Implementation
 });
